76166d8e60fd41f35d4e7c522d3a7249139e783a
ShadowCoo <1289721148@qq.com>
2019-06-08 14:38:28 +0800

    test-shadow

--- server/CloudCourse/src/main/java/CloudCourse/controller/CountController.java
+++ server/CloudCourse/src/main/java/CloudCourse/controller/CountController.java
@@ -1,16 +1,20 @@
 package CloudCourse.controller;
 
 
+import CloudCourse.controller.viewobject.CountVO;
+import CloudCourse.controller.viewobject.ErrorVO;
 import CloudCourse.response.CommonReturnType;
 import CloudCourse.service.CountService;
 import CloudCourse.service.model.CountModel;
 import CloudCourse.service.model.ErrorModel;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("")
@@ -24,7 +28,21 @@ public class CountController extends BaseController {
   @ResponseBody
   public CommonReturnType countCar(@RequestParam(name = "placeId")Integer placeId) throws IOException {
     List<CountModel> countModels = countService.countCar(placeId );
-    return CommonReturnType.create(countModels);
+
+    List<CountVO> countVOList = countModels.stream().map(countModel -> {
+      CountVO countVO = this.convertVOFromModel(countModel);
+      return countVO;
+    }).collect(Collectors.toList());
+
+    return CommonReturnType.create(countVOList);
   }
 
+  private CountVO convertVOFromModel(CountModel countModel){
+    if(countModel == null){
+      return null;
+    }
+    CountVO countVO = new CountVO();
+    BeanUtils.copyProperties(countModel,countVO);
+    return countVO;
+  }
 }
